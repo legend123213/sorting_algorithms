bbe1f876dead709031d6a3063e1413c7e6fa067a
legend123213 <abelwendmu124@gmail.com>
2023-12-23 08:06:43 -0800

    push the mandatory task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,13 +15,13 @@ void swap_ints(int *a, int *b)
 }
 
 /**
- * bubble_sort - Sort an array of integers in ascending order.
+ * bubble_s - Sort an array of integers in ascending order.
  * @array: An array of integers to sort.
  * @size: The size of the array.
  *
  * Description: Prints the array after each swap.
  */
-void bubble_sort(int *array, size_t size)
+void bubble_s(int *array, size_t size)
 {
 	size_t i, len = size;
 	bool bubbly = false;
